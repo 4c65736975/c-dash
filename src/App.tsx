42f8606eeaf3dddabf4f125a5204a06b60d5da7f
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-05-13 16:46:47 +0200

    added copyright header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+/**
+ * App.tsx
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -18,16 +27,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+/**
+ * App.tsx
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
