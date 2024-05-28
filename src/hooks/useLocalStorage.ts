38dd0a0f0a58a13fc57a6973e6d4de34c5bb58cd
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-05-28 21:27:52 +0200

    initial commit

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,59 @@
+/**
+ * useLocalStorage.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+interface IUseLocalStorage {
+  add: <T>(key: string, data: T) => boolean;
+  set: <T>(key: string, data: T) => void;
+  get: <T>(key: string, defaultValue: T) => T | null;
+  has: (key: string) => boolean;
+  remove: (key: string) => void;
+}
+
+const useLocalStorage = (): IUseLocalStorage => {
+  const add = <T>(key: string, data: T): boolean => {
+    if (!has(key)) {
+      set<T>(key, data);
+      return true;
+    }
+
+    return false;
+  };
+
+  const set = <T>(key: string, data: T): void => {
+    localStorage.setItem(key, JSON.stringify(data));
+  };
+
+  const get = <T>(key: string, defaultValue: T): T | null => {
+    const data = localStorage.getItem(key);
+
+    if (data) {
+      return JSON.parse(data); 
+    }
+
+    return defaultValue;
+  };
+
+  const has = (key: string): boolean => {
+    return localStorage.getItem(key) === null;
+  };
+
+  const remove = (key: string) => {
+    localStorage.removeItem(key);
+  };
+
+  return {
+    add,
+    set,
+    get,
+    has,
+    remove
+  };
+};
+
+export default useLocalStorage;
