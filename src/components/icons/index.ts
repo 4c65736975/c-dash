706865fcf04b9fdc7f1de1e56be9963c96aa9632
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-05-28 22:24:07 +0200

    initial commit

---
+++ src/components/icons/index.ts
@@ -0,0 +1,18 @@
+/**
+ * index.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export { default as HumidityRegular } from "./HumidityIcon";
+export { default as PrecipitationRegular } from "./PrecipitationIcon";
+export { default as PressureRegular } from "./PressureIcon";
+export { default as WindRegular } from "./WindIcon";
+// Weather icons
+export { default as CloudyRegular } from "./weather/CloudyIcon";
+export { default as CloudySunRegular } from "./weather/CloudySunIcon";
+export { default as ShowersRegular } from "./weather/ShowersIcon";
+export { default as ThunderstormRegular } from "./weather/ThunderstormIcon";
