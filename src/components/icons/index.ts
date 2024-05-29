5be5db5a44f8b61c1b5676c126f7ee370680970c
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-05-29 16:57:34 +0200

    added new icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -14,5 +14,15 @@ export { default as WindRegular } from "./WindIcon";
 // Weather icons
 export { default as CloudyRegular } from "./weather/CloudyIcon";
 export { default as CloudySunRegular } from "./weather/CloudySunIcon";
+export { default as DrizzleRegular } from "./weather/DrizzleIcon";
+export { default as FogRegular } from "./weather/FogIcon";
+export { default as HailRegular } from "./weather/HailIcon";
+export { default as HailMixedRegular } from "./weather/HailMixedIcon";
+export { default as OvercastRegular } from "./weather/OvercastIcon";
+export { default as RainRegular } from "./weather/RainIcon";
+export { default as ShowersHeavyRegular } from "./weather/ShowersHeavyIcon";
 export { default as ShowersRegular } from "./weather/ShowersIcon";
+export { default as SleetRegular } from "./weather/SleetIcon";
+export { default as SnowRegular } from "./weather/SnowIcon";
+export { default as SunnyRegular } from "./weather/SunnyIcon";
 export { default as ThunderstormRegular } from "./weather/ThunderstormIcon";
