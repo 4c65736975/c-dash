c74c31c8985a6c6b29189d6a844a9cb6c64a03f3
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-05-29 22:35:35 +0200

    initial commit

---
+++ src/constants/Date.const.ts
@@ -0,0 +1,38 @@
+/**
+ * Date.const.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const WEEK_DAY_TO_ABBREVIATE_NAME: string[] = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+];
+
+const MONTH_TO_NAME: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+export {
+  WEEK_DAY_TO_ABBREVIATE_NAME,
+  MONTH_TO_NAME
+};
