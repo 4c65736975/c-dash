d878cb383454287bbd9871025dbb94912931aed1
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-06-05 18:44:37 +0200

    initial commit

---
+++ src/types/Widgets.type.ts
@@ -0,0 +1,10 @@
+/**
+ * Widgets.type.ts
+ *
+ * Copyright (c) 2024 Damian Leśniewski. All Rights Reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export type TWidgetSize = "small" | "medium" | "big";
